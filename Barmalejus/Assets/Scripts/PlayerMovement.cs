cf7f6ea6d029438ab60cb9746979b790c9120c1d
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-19 15:09:49 +0300

    Ambience

--- Barmalejus/Assets/Scripts/PlayerMovement.cs
+++ Barmalejus/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
 
 public class MovePlayer : MonoBehaviour
 {
@@ -14,34 +13,71 @@ public class MovePlayer : MonoBehaviour
     [SerializeField] float gravity = -9.81f;
     [SerializeField] float jumpHeight = 3f;
 
+    //audio
+    private EventInstance playerFootsteps;
+
     Vector3 velocity;
     bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
+        playerFootsteps = AudioManager.instance.CreateEventInstance(FMODEvents.instance.playerFootsteps);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+
+        UpdateSound();
+
+        
+    }
+    private void Movement()
+    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
+        {
             velocity.y = -2;
+        }
+
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+        
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+    private void UpdateSound()
+    {
+        if( controller.velocity.magnitude > 0.0f && isGrounded)
+        {
+            // start footsteps event if it's not already playing
+            PLAYBACK_STATE playbackState;
+            playerFootsteps.getPlaybackState(out playbackState);
+            if(playbackState != PLAYBACK_STATE.PLAYING)
+            {
+                playerFootsteps.start();
+            }
+        }
+        // otherwise stop the footsteps event
+        else
+        {
+            playerFootsteps.stop(STOP_MODE.ALLOWFADEOUT);
+            Debug.Log("Is grounded" + isGrounded);
+        }
     }
 }
