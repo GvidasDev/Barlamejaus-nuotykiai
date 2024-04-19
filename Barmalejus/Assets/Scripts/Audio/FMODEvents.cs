41814f1b4c5096b3afa1a4a139473e88b6b3d2ec
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-19 14:46:30 +0300

    Ambience

---
+++ Barmalejus/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Ambience")]
+    [field: SerializeField] public EventReference ambience { get; private set; }
+
+    [field: Header("Player SFX")]
+    [field: SerializeField] public EventReference playerFootsteps { get; private set; }
+
+    [field: Header("Item PickUp test")]
+    [field: SerializeField] public EventReference itemPickup { get; private set; }
+
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one FMOD Events script in the scene!");
+        }
+        instance = this;
+    }
+}
