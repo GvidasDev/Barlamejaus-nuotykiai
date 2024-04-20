8717fce5966d3d042308e30153978aebec5632c4
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-20 19:41:29 +0300

    Implementing QuestPoint to trigger quest

---
+++ Barmalejus/Assets/Scripts/QuestSystem/QuestPoint.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class QuestPoint : MonoBehaviour
+{
+    [Header("Quest")]
+    [SerializeField] private QuestInfoSO questInfoForPoint;
+
+
+    private bool playerIsNear = false;
+    private string questId;
+    private QuestState currentQuestState;
+
+    private void Awake()
+    {
+        questId = questInfoForPoint.id;
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.questEvents.onQuestStateChange += QuestStateChange;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.onQuestStateChange -= QuestStateChange;
+    }
+
+    // If current held quest id is the same as Quest Point's Id
+    private void QuestStateChange(Quest quest)
+    {
+        if(quest.info.id.Equals(questId))
+        {
+            currentQuestState = quest.state;
+            Debug.Log("Quest with id: " + questId + " updated to state: " + currentQuestState);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsNear = false;
+        }
+    }
+
+}
