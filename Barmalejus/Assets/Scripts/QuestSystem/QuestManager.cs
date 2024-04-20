f280c03ef9ff09f7fc293825d12f4e8326e2cfd8
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-20 18:55:19 +0300

    More progress towards quest system

--- Barmalejus/Assets/Scripts/QuestSystem/QuestManager.cs
+++ Barmalejus/Assets/Scripts/QuestSystem/QuestManager.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Dictionary<string, Quest> questMap;
+    private void Awake()
     {
-        
+        questMap = CreateQuestMap();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        GameEventsManager.instance.questEvents.onStartQuest += StartQuest;
+        GameEventsManager.instance.questEvents.onAdvanceQuest += AdvanceQuest;
+        GameEventsManager.instance.questEvents.onFinishQuest += FinishQuest;
     }
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.onStartQuest -= StartQuest;
+        GameEventsManager.instance.questEvents.onAdvanceQuest -= AdvanceQuest;
+        GameEventsManager.instance.questEvents.onFinishQuest -= FinishQuest;
+    }
+
+    private void Start()
+    {
+        // broadcast the initial state of all quests on startup
+        foreach(Quest quest in questMap.Values)
+        {
+            GameEventsManager.instance.questEvents.QuestStateChange(quest);
+        }
+    }
+
+    private void StartQuest(string id)
+    {
+        // TODO - start the quest
+        Debug.Log("Start Quest: " + id);
+    }
+
+    private void AdvanceQuest(string id)
+    {
+        // TODO - advance the quest
+        Debug.Log("Advance Quest: " + id);
+    }
+
+    private void FinishQuest(string id)
+    {
+        // TODO - finish the quest
+        Debug.Log("Finish Quest: " + id);
+    }
+
+
+    private Dictionary<string, Quest> CreateQuestMap()
+    {
+        // Loads all QuestInfoSO scriptable objects under the Assets/Resources/Quests folder
+        QuestInfoSO[] allQuests = Resources.LoadAll<QuestInfoSO>("Quests");
+        // Create the quest map
+        Dictionary<string, Quest> idToQuestMap = new Dictionary<string, Quest>();
+        foreach(QuestInfoSO questInfo in allQuests)
+        {
+            if(idToQuestMap.ContainsKey(questInfo.id))
+            {
+                Debug.LogWarning("Duplicate ID was found when creating quest map: " + questInfo.id);
+            }
+            idToQuestMap.Add(questInfo.id, new Quest(questInfo));
+        }
+        return idToQuestMap;
+    }
+
+    private Quest GetQuestById(string id)
+    {
+        Quest quest = questMap[id];
+        if(quest == null)
+        {
+            Debug.LogError("ID not found in Quest Map: " + id);
+        }
+        return quest;
+    }
+
 }
