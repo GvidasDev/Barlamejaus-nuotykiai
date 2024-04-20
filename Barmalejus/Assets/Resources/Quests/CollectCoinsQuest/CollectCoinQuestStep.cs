8f0ffee502b4b0cf129b81dbc2373fefc6ed44f0
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-20 15:52:06 +0300

    Setting up quest system

---
+++ Barmalejus/Assets/Resources/Quests/CollectCoinsQuest/CollectCoinQuestStep.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoinQuestStep : QuestStep
+{
+
+    private int coinsCollected = 0;
+    private int coinsToCollect = 5;
+
+
+
+
+}
---
+++ Barmalejus/Assets/Scripts/QuestSystem/QuestInfoSO.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(fileName = "QuestInfoSO", menuName = "ScriptableObjects/QuestInfoSO", order = 1)]
+public class QuestInfoSO : ScriptableObject
+{
+    [field: SerializeField] public string id { get; private set; }
+
+    // What name will appear in game itself
+    [Header("General")]
+    public string displayName;
+
+    // Required level and what quests have to be finished before starting a new one
+    [Header("Requirements")]
+    public int levelRequirement;
+    public QuestInfoSO[] questPrerequisites;
+
+    [Header("Steps")]
+    public GameObject[] questStepPrefabs;
+
+    [Header("Rewards")]
+    public int playerLevel;
+
+    // ensure the id is always the name of the Scriptable Object asset
+    private void OnValidate()
+    {
+        #if UNITY_EDITOR
+        id = this.name;
+        UnityEditor.EditorUtility.SetDirty(this);
+        #endif
+    }
+
+}
