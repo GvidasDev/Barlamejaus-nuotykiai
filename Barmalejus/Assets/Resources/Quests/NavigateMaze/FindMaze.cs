7cca81cd424557d124de9f2dfab601f350884a80
Voopieq <domantas.kelmanas2003@gmail.com>
2024-04-23 23:57:00 +0300

    Maze, changing brightness
    -World light changes when entering/leaving maze making the process of finding engine part more spooky

---
+++ Barmalejus/Assets/Resources/Quests/NavigateMaze/FindMaze.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class FindMaze : QuestStep
+{
+
+    private void Start()
+    {
+        string status = "Find the maze!";
+        Debug.Log(status);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            FinishQuestStep();
+        }
+    }
+
+    protected void UpdateState()
+    {
+        // no state is needed for this quest (would require if there was e.g incrementing integer)
+    }
+    protected override void SetQuestStepState(string state)
+    {
+        // no state is needed for this quest (would require if there was e.g incrementing integer)
+    }
+}
--- Barmalejus/Assets/Scripts/TriggerZoneDetection.cs
+++ Barmalejus/Assets/Scripts/TriggerZoneDetection.cs
@@ -4,21 +4,77 @@ using UnityEngine;
 
 public class TriggerZoneDetection : MonoBehaviour
 {
+    private GameObject sun;
+    private Light sunLight;
+    private float initialIntensity;
+    private float finalIntensity = 0.1f;
+    public float duration = 2f;
+    public float currentLerpTime = 0f;
+    private bool shouldRotate = false;
+    public float initialSkyboxIntensity;
+    public float finalSkyboxIntensity = 0f;
 
+    private void Start()
+    {
+        sun = GameObject.Find("Spot Light");
+        sunLight = sun.GetComponent<Light>();
+        initialIntensity = sunLight.intensity;
+        initialSkyboxIntensity = RenderSettings.ambientIntensity;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Pickable"))
+        if(other.CompareTag("Player"))
         {
-            string itemName = other.gameObject.name;
-            Debug.Log(itemName + " has been delivered!");
+            shouldRotate = true;
+            currentLerpTime = 0f;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Pickable"))
+        if (other.CompareTag("Player"))
         {
-            string itemName = other.gameObject.name;
-            Debug.Log(itemName + " has been taken away!");
+            shouldRotate = false;
+            currentLerpTime = 0f;
+        }
+    }
+    void Update()
+    {
+        if (shouldRotate)
+        {
+            Darken();
+        }
+        else
+        {
+            Brighten();
+        }
+        
+    }
+    void Darken()
+    {
+            if (currentLerpTime < duration)
+            {
+                currentLerpTime += Time.deltaTime;
+                if (currentLerpTime > duration)
+                {
+                    currentLerpTime = duration;
+                }
+                float t = currentLerpTime / duration;
+                sunLight.intensity = Mathf.Lerp(initialIntensity, finalIntensity, t);
+                RenderSettings.ambientIntensity = Mathf.Lerp(initialSkyboxIntensity, finalSkyboxIntensity, t);
+            }
+    }
+    void Brighten()
+    {
+            if (currentLerpTime < duration)
+            {
+                currentLerpTime += Time.deltaTime;
+                if (currentLerpTime > duration)
+                {
+                    currentLerpTime = duration;
+                }
+                float t = 1 - (currentLerpTime / duration);
+                sunLight.intensity = Mathf.Lerp(initialIntensity, finalIntensity, t);
+                RenderSettings.ambientIntensity = Mathf.Lerp(initialSkyboxIntensity, finalSkyboxIntensity, t);
         }
     }
 }
