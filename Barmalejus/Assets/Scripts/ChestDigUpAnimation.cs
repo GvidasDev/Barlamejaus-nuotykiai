f16314d7a330ce98ecf2cea4759bc28377773f53
Voopieq <domantas.kelmanas2003@gmail.com>
2024-05-12 00:02:57 +0300

    Chest open animation and sound
    -Made a chest opening animation when on trigger and F key is pressed

---
+++ Barmalejus/Assets/Scripts/ChestDigUpAnimation.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestDigUpAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator chestDigUp;
+    [SerializeField] private Animator chestOpen;
+    [SerializeField] private GameObject interactionPrompt;
+
+    private bool isPlayerNear = false;
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.inputEvents.onSubmitPressed += SubmitPressed;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.inputEvents.onSubmitPressed -= SubmitPressed;
+    }
+
+    private void Awake()
+    {
+        interactionPrompt.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        chestDigUp.SetTrigger("DigUp");
+        interactionPrompt.SetActive(true);
+        isPlayerNear = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        interactionPrompt.SetActive(false);
+        isPlayerNear = false;
+    }
+
+    private void SubmitPressed()
+    {
+        if(isPlayerNear)
+        {
+            chestOpen.SetTrigger("ChestOpen");
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.chestOpen, this.transform.position);
+            interactionPrompt.SetActive(false);
+            Destroy(this.gameObject);
+        }
+    }
+}
